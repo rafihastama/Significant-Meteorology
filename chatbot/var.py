a811fedd541cac2392a205e4d166e49201b2cb4b
je <envyxbot@gmail.com>
2023-07-05 01:37:54 +0700

    change attrbute condition to pattern matching attribute for attribute condition

--- chatbot/var.py
+++ chatbot/var.py
@@ -6,7 +6,7 @@ class var:
         'terkini', 'status', 'seluruh', 'field', 'fl', 'date', 'lebih', 'kurang', 'dari', 'lokasi', 'titik',
         'flight information', 'gunung', 'posisi', 'gunung', 'diobservasi', 'polygon', 'flight', 'level', 'waktu',
         'meter', 'feet', 'movement', 'speed', 'intensitivity', 'status', 'kaki', 'meter', 'lintang', 'jam', 'hingga',
-        'pergerakan', 'awan', 'diatas', 'dibawah'
+        'pergerakan', 'awan', 'diatas', 'dibawah', 'tanggal'
     ]
 
     IGNORE_PATTERN = [
@@ -58,51 +58,64 @@ class var:
     }
 
     condition = {
-        (r'(untuk)', r'dengan'): "WHERE"
+        (r'(untuk)', r'dengan', r'yang'): "WHERE"
     }
-
-    # attribute field for condition
-    attribute_condition = {
-        r"(info\ssigmet\sterkini|info\ssigmet\sterbaru)": "release_date",
-        r"ketinggian\sawan\sabu\svulkanik\s\w+\s\d+\s(meter)": "meter",
-        r"ketinggian\sawan\sabu\svulkanik\s\w+\s\d+\s(kaki)": "feet",
-        r"untuk\sflight\slevel\s(\d{3})|untuk\sfl\s(\d{3})": "flight_level",
-        r"lintang\s([nsew]\d{4,5}\s[nsew]\d{4,5})": "polygon",
-        r"waktu\svalid\sdari\sjam": "from_valid_date",
-        r"jam\s(\d{2}:\d{2})\s(\d{2}:\d{2})|jam\s(\d{2}:\d{2})\shingga\s(\d{2}:\d{2})": "to_valid_date",
-        r"sigmet\skode\s(\d{2})|sigmet\s(\d{2})": "sigmet_code"
-    }
-
     operator = {
         r"diatas": ">",
         r"dibawah": "<",
         r"sama\sdengan": "=",
-        r"seluruh\sfield": "*",
         r"lintang": "LIKE",
     }
 
-    data = {
+    pattern_matching_attribute = {
+        "tanggal dikeluarkan": {
+            "pattern": r"untuk\stanggal\s(\d{2}\-\d{2}\-\d{4})",
+            "data": r"(\d{2}\-\d{2}\-\d{4})",
+            "attribute": "release_date",
+            "default_operator": "="
+        },
         "sigmet code": {
             "pattern": r"sigmet\skode\s(\d{2})|sigmet\s(\d{2})",
             "attribute": "sigmet_code",
             "data": r"(\d{2})",
-            "operator": "="
+            "default_operator": "="
         },
-        "ketinggian": r"(\d{3,6})\smeter|(\d{3,6})\skaki",
-        "lintang": r"([nsew]\d{4,5}\s[nsew]\d{4,5})",
         "flight level": {
             "pattern": r"untuk\sflight\slevel\s(\d{3})|untuk\sfl\s(\d{3})",
             "attribute": "flight_level",
             "data": r"(\d{3})",
-            "operator": "="
+            "default_operator": "="
         },
-        "valid": r"(\d{2}:\d{2})\s(\d{2}:\d{2})|(\d{2}:\d{2})\shingga\s(\d{2}:\d{2})|(\d{2}:\d{2})\s-\s(\d{2}:\d{2})",
-        "current date": {
+        "data terbaru": {
             "pattern": r"(info\ssigmet\sterkini|info\ssigmet\sterbaru)",
             "attribute": "release_date",
             "data": "DATE(convert_tz(now(), @@session.time_zone, '+07:00'))",
-            "operator": "="
-        }
+            "default_operator": "="
+        },
+        "waktu valid": {
+            "pattern": r"(\d{2}:\d{2})\s(\d{2}:\d{2})|(\d{2}:\d{2})\shingga\s(\d{2}:\d{2})|(\d{2}:\d{2})\s-\s(\d{2}:\d{2})",
+            "attribute": ['from_valid_date', 'to_valid_date'],
+            "data": r"(\d{2}:\d{2})\s(\d{2}:\d{2})|(\d{2}:\d{2})\shingga\s(\d{2}:\d{2})|(\d{2}:\d{2})\s-\s(\d{2}:\d{2})",
+            "default_operator": "valid_date"
+        },
+        "ketinggian dalam bentuk meter": {
+            "pattern": r"ketinggian\sawan\sabu\svulkanik\s\w+\s\d+\s(meter)",
+            "attribute": "meter",
+            "data": r"(\d{3,6})\smeter",
+            "default_operator": None
+        },
+        "ketinggian dalam bentuk kaki": {
+            "pattern": r"ketinggian\sawan\sabu\svulkanik\s\w+\s\d+\s(kaki)",
+            "attribute": "feet",
+            "data": r"(\d{3,6})\skaki",
+            "default_operator": None
+        },
+        "lintang": {
+            "pattern": r"lintang\s([nsew]\d{4,5}\s[nsew]\d{4,5})",
+            "attribute": "polygon",
+            "data": r"([nsew]\d{4,5}\s[nsew]\d{4,5})",
+            "default_operator": None
+        },
     }
 
     """
@@ -113,43 +126,43 @@ class var:
     # pattern regex untuk pengecekan aturan produksi
     TAMPILKAN = r"^(tampilkan)"
     PATTERN_RULE_TAMPILKAN = {
-        "field": r"tampilkan\s(seluruh\sfield|kode\ssigmet|waktu\svalid|lokasi\sdikeluarkannya|lokasi\sgunung|posisi\sgunung|waktu\sdiobservasi|polygon|flight\slevel|meter|kaki|pergerakan\sabu\svulkanik|kecepatan\sabu\svulkanik|intensitivity|status|jam\ssigmet\sdikeluarkan|tanggal\ssigmet\sdikeluarkan)+\suntuk\s(info\ssigmet\sterkini|info\ssigmet\sterbaru|kode\ssigmet\s\d{2})",
+        "field": r"tampilkan\s((seluruh\sfield)|(kode\ssigmet)((,\s)|(\sdan\s))?|(waktu\svalid)((,\s)|(\sdan\s))?|(lokasi\sdikeluarkannya)((,\s)|(\sdan\s))?|(lokasi\sgunung((,\s)|(\sdan\s))?)|(posisi\sgunung)((,\s)|(\sdan\s))?|(waktu\sdiobservasi)((,\s)|(\sdan\s))?|(polygon)((,\s)|(\sdan\s))?|(flight\slevel)((,\s)|(\sdan\s))?|(meter|kaki)((,\s)|(\sdan\s))?|(pergerakan\sabu\svulkanik)((,\s)|(\sdan\s))?|(kecepatan\sabu\svulkanik)((,\s)|(\sdan\s))?|(intensitas\sabu\svulkanik)((,\s)|(\sdan\s))?|(status)((,\s)|(\sdan\s))?|(jam\ssigmet\sdikeluarkan)((,\s)|(\sdan\s))?|(tanggal\ssigmet\sdikeluarkan)((,\s)|(\sdan\s))?)+\suntuk\s(info\ssigmet\sterkini|info\ssigmet\sterbaru|kode\ssigmet\s\d{2}|tanggal\s(\d{2}\-\d{2}\-\d{4}))",
         "ketinggian": r"ketinggian\sawan\sabu\svulkanik\s(diatas|dibawah)\s(\d{,6})\s(meter|kaki)",
         "flight level": r"untuk\sflight\slevel\s(\d{3})|untuk\sfl\s(\d{3})",
         "lintang": r"lintang\s([nsew]\d{4,5}\s[nsew]\d{4,5})",
         "valid": r"jam\s(\d{2}:\d{2})\s(\d{2}:\d{2})|jam\s(\d{2}:\d{2})\shingga\s(\d{2}:\d{2})",
-        "penyebaran abu vulkanik": r"wilayah\spenyebaran\sabu\svulkanik\s\w+\s(info\ssigmet\sterkini|info\ssigmet\sterbaru|kode\ssigmet\s\d{2})",
+        "penyebaran abu vulkanik": r"wilayah\spenyebaran\sabu\svulkanik\s\w+\s(info\ssigmet\sterkini|info\ssigmet\sterbaru|kode\ssigmet\s\d{2}|tanggal\s(\d{2}\-\d{2}\-\d{4}))",
     }
 
     BERAPA = r"^(berapa)"
     PATTERN_RULE_BERAPA = {
-        'valid': r"waktu\svalid\suntuk\s(info\ssigmet\sterkini|info\ssigmet\sterbaru|kode\ssigmet\s\d{2})",
-        'ketinggian abu vulkanik': r"ketinggian\sabu\svulkanik\suntuk\s(info\ssigmet\sterkini|info\ssigmet\sterbaru|kode\ssigmet\s\d{2})",
-        'kecepatan abu vulkanik': r"kecepatan\sabu\svulkanik\suntuk\s(info\ssigmet\sterkini|info\ssigmet\sterbaru|kode\ssigmet\s\d{2})"
+        'valid': r"waktu\svalid\suntuk\s(info\ssigmet\sterkini|info\ssigmet\sterbaru|kode\ssigmet\s\d{2}|tanggal\s(\d{2}\-\d{2}\-\d{4}))",
+        'ketinggian abu vulkanik': r"ketinggian\sabu\svulkanik\suntuk\s(info\ssigmet\sterkini|info\ssigmet\sterbaru|kode\ssigmet\s\d{2}|tanggal\s(\d{2}\-\d{2}\-\d{4}))",
+        'kecepatan abu vulkanik': r"kecepatan\sabu\svulkanik\suntuk\s(info\ssigmet\sterkini|info\ssigmet\sterbaru|kode\ssigmet\s\d{2}|tanggal\s(\d{2}\-\d{2}\-\d{4}))"
     }
 
     DIMANA = r"^(dimana)"
     PATTERN_RULE_DIMANA = {
-        'lokasi gunung': r'lokasi\sgunung\suntuk\s(info\ssigmet\sterkini|info\ssigmet\sterbaru|kode\ssigmet\s\d{2})',
-        'lokasi flight information': r'lokasi\sdikeluarkannya\suntuk\s(info\ssigmet\sterkini|info\ssigmet\sterbaru|kode\ssigmet\s\d{2})',
-        'penyebaran abu vulkanik': r'penyebaran\sabu\svulkanik\suntuk\s(info\ssigmet\sterkini|info\ssigmet\sterbaru|kode\ssigmet\s\d{2})'
+        'lokasi gunung': r'lokasi\sgunung\suntuk\s(info\ssigmet\sterkini|info\ssigmet\sterbaru|kode\ssigmet\s\d{2}|tanggal\s(\d{2}\-\d{2}\-\d{4}))',
+        'lokasi flight information': r'lokasi\sdikeluarkannya\suntuk\s(info\ssigmet\sterkini|info\ssigmet\sterbaru|kode\ssigmet\s\d{2}|tanggal\s(\d{2}\-\d{2}\-\d{4}))',
+        'penyebaran abu vulkanik': r'penyebaran\sabu\svulkanik\suntuk\s(info\ssigmet\sterkini|info\ssigmet\sterbaru|kode\ssigmet\s\d{2}|tanggal\s(\d{2}\-\d{2}\-\d{4}))'
     }
 
     KAPAN = r"^(kapan)"
     PATTERN_RULE_KAPAN = {
-        "dikeluarkan": r'dikeluarkannya\suntuk\s(info\ssigmet\sterkini|info\ssigmet\sterbaru|kode\ssigmet\s\d{2})',
-        "diobservasi": r'diobservasinya\suntuk\s(info\ssigmet\sterkini|info\ssigmet\sterbaru|kode\ssigmet\s\d{2})'
+        "dikeluarkan": r'dikeluarkannya\suntuk\s(info\ssigmet\sterkini|info\ssigmet\sterbaru|kode\ssigmet\s\d{2}|tanggal\s(\d{2}\-\d{2}\-\d{4}))',
+        "diobservasi": r'diobservasinya\suntuk\s(info\ssigmet\sterkini|info\ssigmet\sterbaru|kode\ssigmet\s\d{2}|tanggal\s(\d{2}\-\d{2}\-\d{4}))'
     }
 
     APA = r"^(apa)"
     PATTERN_RULE_APA = {
-        "status": r"status\suntuk\s(info\ssigmet\sterkini|info\ssigmet\sterbaru|kode\ssigmet\s\d{2})",
-        "intesitas abu vulkanik": r"intensitas\sabu\svulkanik\suntuk\s(info\ssigmet\sterkini|info\ssigmet\sterbaru|kode\ssigmet\s\d{2})|"
-                                  r"intensitivity\sabu\svulkanik\suntuk\s(info\ssigmet\sterkini|info\ssigmet\sterbaru|kode\ssigmet\s\d{2})"
+        "status": r"status\suntuk\s(info\ssigmet\sterkini|info\ssigmet\sterbaru|kode\ssigmet\s\d{2}|tanggal\s(\d{2}\-\d{2}\-\d{4}))",
+        "intesitas abu vulkanik": r"intensitas\sabu\svulkanik\suntuk\s(info\ssigmet\sterkini|info\ssigmet\sterbaru|kode\ssigmet\s\d{2}|tanggal\s(\d{2}\-\d{2}\-\d{4}))|"
+                                  r"intensitivity\sabu\svulkanik\suntuk\s(info\ssigmet\sterkini|info\ssigmet\sterbaru|kode\ssigmet\s\d{2}|tanggal\s(\d{2}\-\d{2}\-\d{4}))"
     }
 
     ARAH = r"(kearah)"
     PATTERN_RULE_ARAH = {
-        "arah abu vulkanik": r"abu\svulkanik\suntuk\s(info\ssigmet\sterkini|info\ssigmet\sterbaru|kode\ssigmet\s\d{2})"
+        "arah abu vulkanik": r"abu\svulkanik\suntuk\s(info\ssigmet\sterkini|info\ssigmet\sterbaru|kode\ssigmet\s\d{2}|tanggal\s(\d{2}\-\d{2}\-\d{4}))"
     }
 
