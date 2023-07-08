68ccf33a86293287e7820db952fd1fa684460ffc
Lucius Caelum <abidrafinurhastama@gmail.com>
2023-07-08 14:10:55 +0700

    modify special formatting for key

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -191,7 +191,7 @@ function formatBotResponse (data) {
       formattedValue = formattedValue.replaceAll(' - ', '\n')
     }
 
-    if (key === 'polygon' && data[key] != null) {
+    if (key === 'polygon ') {
       formattedValue = formattedValue.replaceAll(' - ', '\n')
     }
 
